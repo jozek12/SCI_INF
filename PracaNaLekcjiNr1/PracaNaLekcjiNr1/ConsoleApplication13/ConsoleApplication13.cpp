6471547c32fef932d51133b4cb4274b57c0e0b79
Michał <mishka12@o2.pl>
2021-11-07 13:17:39 +0100

    gotowy program

--- PracaNaLekcjiNr1/PracaNaLekcjiNr1/ConsoleApplication13/ConsoleApplication13.cpp
+++ PracaNaLekcjiNr1/PracaNaLekcjiNr1/ConsoleApplication13/ConsoleApplication13.cpp
@@ -1,22 +1,26 @@
 ﻿#include <iostream>
 #include <string>
 using namespace std;
-string podstawieniowy(string tekstt)
+string podstawieniowy(string tekstt,int klucz,string temp_string)
 {
-	int d;
-	cout << "podaj liczbe do przesuwania:" << endl;
-	cin >> d;
 	for (int i = 0; i < tekstt.length(); i++)
 	{
-		if (tekstt[i] > 96 && tekstt[i] < 123)
+		if (tekstt[i] > 96)
 		{
-				tekstt[i] += d;
-				if (tekstt[i] > 122)
-				{
-					tekstt[i] = tekstt[i] % 123 + 97;
-				}
+			unsigned char temp;
+			temp = tekstt[i] + klucz;
+			if (temp >= 123)
+			{
+				temp = (temp % 123) + 97;
+			}
+			temp_string += temp;
+		}
+		else
+		{
+			temp_string += tekstt[i];
 		}
 	}
+	return temp_string;
 	return tekstt;
 }
 string przestawieniowy(string tekstt)
@@ -29,32 +33,74 @@ string przestawieniowy(string tekstt)
 
 	return tekstt;
 }
+void deszyfrowanie_cezara(string tekstt , int d , string temp_string)
+{
+	for (int j = 1; j < 27; j++)
+	{
+		d = j;
+		for (int i = 0; i < tekstt.length(); i++)
+		{
+			if (tekstt[i] > 96)
+			{
+				if (tekstt.length() == i)
+				{
+					temp_string.push_back(' ');
+				}
+				unsigned char temp;
+				temp = tekstt[i] - d;
+				if (temp <= 96)
+				{
+					char x = (tekstt[i] % 97) + 123;
+					x -= d;
+					temp = x;
+				}
+				temp_string += temp;
+
+			}
+			else
+			{
+				temp_string += tekstt[i];
+			}
+		}
+	}
+	cout << "mozliwe deszyfrowanie: " << temp_string;
+}
 int main()
 {
-	string tekst , tekst2;
+	string tekst, tekst2, tekst3;
+	int d;
 	int a;
+	int c = 1;
 		cout << "podaj ciag znakow(z malych liter)" << endl;
 		getline(cin, tekst);
+		while (c == 1)
+		{
 		cout << "podaj opcje szyfrowania:\n1. szyfr podstawieniowy\n2. Szyfr przestawieniowy \n3. Szyfr podstawieniowy i szyfr przestawieniowy. \n4. Odszyfruj teskt.";
 		cin >> a;
-		if (a == 1)
-		{
-			tekst2 = podstawieniowy(tekst);
-			cout << "zaszyfrowany tekst to:" << tekst2 << endl;
-		}
-		if (a == 2)
-		{
-			tekst2 = przestawieniowy(tekst);
-			cout << "zaszyfrowany tekst to:" << tekst2 << endl;
-		}
-		if (a == 3)
-		{
-			tekst2 = podstawieniowy(tekst);
-			tekst2 = przestawieniowy(tekst2);
-			cout << "zaszyfrowany tekst to:" << tekst2 << endl;
-		}
-		if (a == 4)
-		{
+			if (a == 1)
+			{
+				cout << "podaj liczbe do podstawiania:(do 26)" << endl;
+				cin >> d;
+				tekst2 = podstawieniowy(tekst, d , tekst3);
+				cout << "zaszyfrowany tekst to:" << tekst2 << endl;
+			}
+			if (a == 2)
+			{
+				tekst2 = przestawieniowy(tekst);
+				cout << "zaszyfrowany tekst to:" << tekst2 << endl;
+			}
+			if (a == 3)
+			{
+				cout << "podaj liczbe do podstawiania:(do 26)" << endl;
+				cin >> d;
+				tekst2 = podstawieniowy(tekst, d ,tekst3);
+				tekst2 = przestawieniowy(tekst2);
+				cout << "zaszyfrowany tekst to:" << tekst2 << endl;
+			}
+			if (a == 4)
+			{
 
-	}
+				deszyfrowanie_cezara(tekst2, d, tekst3);
+			}
+		}
 }
